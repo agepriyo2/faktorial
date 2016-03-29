30497d38f0dcad49fc730a632deea22ad19f1969
Age Priyo Rantono <agepriyo@hotmail.com>
2016-03-29 09:53:26 +0700

    Added files via upload

---
+++ Jurnal2new.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jurnal2new;
+
+/**
+ *
+ * @author Praktikum
+ */
+public class Jurnal2new {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 6;
+        int j;
+        int k;
+        for ( j = 1; j <= i; j++) {
+            System.out.println("");
+            for (k = 0; k < j; k++) {
+                System.out.print("0");
+            }
+            
+        }
+        System.out.println("");
+        
+    }
+    }
+    
+}
